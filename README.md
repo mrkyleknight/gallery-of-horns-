4ce558e082420a22e2adb4aab739eea9846a92f3
Kyle Knight <knightkyle@outlook.com>
2023-06-06 19:27:19 -0700

    Upload lab 2

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
+Number and name of feature: ________________________________
+
+Estimate of time needed to complete: _____
+
+Start time: _____
+
+Finish time: _____
+
+Actual time needed to complete: _____
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/Main.js
+++ src/Main.js
@@ -1,20 +1,25 @@
 
 import React from 'react';
 import HornedBeast from './HornedBeast';
+import data from './data.json';
 
 class Main extends React.Component {
   render() {
     return (
-      <>
-      <h2> "A beast does not know that he is a beast, and the nearer a man gets to being a beast, the less he knows it."
+      <main>
+        {data.map(animalObj => {
+          return <HornedBeast animalName={animalObj._id} image_url={animalObj.image_url} />
+        })}
+
+      {/* <h2> "A beast does not know that he is a beast, and the nearer a man gets to being a beast, the less he knows it."
         
 
 George MacDonald</h2>
       <HornedBeast animalName='Dog' description='Dog with horns' image_url={'https://i.pinimg.com/originals/f5/0f/19/f50f1957c7a1b902471b6f6ad83d7684.jpg'} />
       <HornedBeast animalName='Cat' description='Cat with horns' image_url={'https://uploads.neatorama.com/images/posts/485/73/73485/1404320986-0.jpg'} />
       <HornedBeast animalName='Bird' description='Bird with horns' image_url={'https://dannybrownphotography.files.wordpress.com/2013/02/djb_rvl_2013_01831.jpg'} />
-      <HornedBeast animalName='Rabbit' description='Rabbit with horns' image_url={'https://t3.ftcdn.net/jpg/05/35/65/92/360_F_535659252_n2hQBI4kHqEyATPDkAU59GrXbqWqgeFE.jpg'} />
-      </>
+      <HornedBeast animalName='Rabbit' description='Rabbit with horns' image_url={'https://t3.ftcdn.net/jpg/05/35/65/92/360_F_535659252_n2hQBI4kHqEyATPDkAU59GrXbqWqgeFE.jpg'} /> */}
+      </main>
 
     ) 
   }
